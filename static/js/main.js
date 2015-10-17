52105dfde99a9dbb4e69bd03671d77b7790fd135
Florent Desjardins <florent.desjardins@gmail.com>
2015-10-17 17:39:00 +0200

    from jquery to react

--- static/js/main.js
+++ static/js/main.js
@@ -1,55 +1,74 @@
-(function($){
-    
-    var App = {
+var RestaurandComponent = React.createClass({
+    getRestaurants: function() {
+        $.ajax({
+            url: 'https://restaurand-api.herokuapp.com/api/v1/restaurants',
+            cache: false,
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function(data) {
+                this.setState({
+                    loaded: true,
+                    data: _.shuffle(data)
+                });
+            }.bind(this)
+        });
+    },
+    componentDidMount: function() {
+        this.getRestaurants();
+    },
+    getInitialState: function() {
+        return {
+            loaded: false,
+            data: []
+        };
+    },
+    handleClick: function() {
+        this.getRestaurants();
+    },
+    render: function() {
+        if (!this.state.loaded) {
+            return (
+                <div className="ajax-loader">
+                    <img src="static/images/ajax-loader.gif" />
+                </div>
+            );
+        }
 
-        init: function() {
-            this.getRestaurants();
-            $('button').on('click', this.shuffle);
-        },
+        var rows = this.state.data.map(function(restaurant, index) {
+            if (index === 0) {
+                return (
+                    <tr key={restaurant.id} className="active">
+                        <td>{restaurant.name}</td>
+                    </tr>
+                );
+            }
+            return (
+                <tr key={restaurant.id}>
+                    <td>{restaurant.name}</td>
+                </tr>
+            );
+        });
 
-        getRestaurants: function(params) {
-            $.ajax({
-                url: 'https://restaurand-api.herokuapp.com/api/v1/restaurants',
-                cache: false,
-                type: 'GET',
-                dataType: 'json',
-                contentType: 'application/json',
-                complete: function() {
-                    $('#ajax-loader').remove();
-                },
-                success: function(data) {
-                    App.showRestaurantsList(data);
-                },
-            });
-        }, 
-        
-        showRestaurantsList: function(data) {
-            var source = $('#restaurants-list').html();
-            var template = Handlebars.compile(source);
-            var context = {data};
-            $('tbody').append(template(context));
-        },
+        return (
+            <div className="restaurand">
+                <button onClick={this.handleClick} className="btn btn-primary" type="button">Randomiser</button>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Liste des restaurants</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+});
 
-        shuffle: function(e) {
-            e.preventDefault();
-            e.stopPropagation();
-
-            var tbody = $('tbody');
-            var shuffled = _.shuffle($('tbody tr'));
-            tbody.empty();
-
-            $.each(shuffled, function(index, el) {
-                tbody.append($(el));
-            });
-
-            $('tbody tr.first-row').removeClass('first-row');
-            $('tbody tr:first-child').addClass('first-row');
-
-            return false; 
-        },
-    };
-
-    App.init();    
-
-  
-})(jQuery);
+ReactDOM.render(
+    <RestaurandComponent />,
+    document.getElementById('restaurandapp')
+);
